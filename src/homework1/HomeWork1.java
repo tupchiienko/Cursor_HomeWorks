183f1d8677b8928b8c7a24d36b6fac4b5a352be0
typchiienko <vt.progr@gmail.com>
2021-03-20 15:22:25 +0200

    Home Work 1

--- src/homework1/HomeWork1.java
+++ src/homework1/HomeWork1.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 public class HomeWork1 {
     public static void main(String[] args) {
 
+        // 1
         System.out.println("1. DESC sorting.");
 
         Integer[] integerArray = {2, 3, 1, 7, 11};
@@ -14,13 +15,13 @@ public class HomeWork1 {
         Arrays.sort(integerArray, Collections.reverseOrder());
         System.out.println("Sorted DESC: " + Arrays.toString(integerArray));
 
+        //2
         System.out.println("\n2. Sum of positive numbers.");
 
         integerArray = new Integer[]{2, 3, 1, 7, 11, -2, -5, -1};
+        int sum = 0;
 
         System.out.println("Numbers: " + Arrays.toString(integerArray));
-
-        int sum = 0;
 
         for (Integer integer : integerArray) {
             if (integer > 0) {
@@ -30,20 +31,22 @@ public class HomeWork1 {
 
         System.out.println("Sum of positive: " + sum);
 
+        //3
         System.out.println("\n3. Get average of numbers.");
 
-        integerArray = new Integer[]{1,2,4,1};
+        integerArray = new Integer[]{1, 2, 4, 1};
+        double avg = 0;
 
         System.out.println("Numbers: " + Arrays.toString(integerArray));
 
-        int avg = 0;
         for (Integer integer : integerArray) {
             avg += integer;
         }
-        avg /= integerArray.length;
 
+        avg /= integerArray.length;
         System.out.println("Average: " + avg);
 
+        //4
         System.out.println("\n4. Replace duplicates by 0.");
 
         integerArray = new Integer[]{3, 2, 3, 1, 4, 2, 8, 3};
