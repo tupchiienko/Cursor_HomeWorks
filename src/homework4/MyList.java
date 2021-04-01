19b4d41a6aed5fce665161a8a6345b04e58bc2e7
typchiienko <vt.progr@gmail.com>
2021-04-01 14:11:20 +0300

    Home Work 4 (Fixed parametrized type of MyList)

--- src/homework4/MyList.java
+++ src/homework4/MyList.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-public class MyList<T extends Comparable<T>> {
+public class MyList<T extends Comparable<? super T>> {
     private final ArrayList<T> list;
 
     public MyList() {
