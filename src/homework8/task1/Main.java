6db10f71f5dedffa5e8c5e70814bbdada1dfdaf5
typchiienko <vt.progr@gmail.com>
2021-04-16 09:26:31 +0300

    Home Work 8 (Task 1 specified exceptions type)

--- src/homework8/task1/Main.java
+++ src/homework8/task1/Main.java
@@ -6,22 +6,22 @@ public class Main {
     public static void main(String[] args) {
         try {
             throw new ExceptionA("Caught ExceptionA");
-        } catch (Exception exception) {
+        } catch (ExceptionA exception) {
             System.out.println(exception.getMessage());
         }
         try {
             throw new ExceptionB("Caught ExceptionB");
-        } catch (Exception exception) {
+        } catch (ExceptionB exception) {
             System.out.println(exception.getMessage());
         }
         try {
             throw new NullPointerException("Caught NullPointerException");
-        } catch (Exception exception) {
+        } catch (NullPointerException exception) {
             System.out.println(exception.getMessage());
         }
         try {
             throw new IOException("Caught IOException");
-        } catch (Exception exception) {
+        } catch (IOException exception) {
             System.out.println(exception.getMessage());
         }
     }
