689288148b2e33b26d37669cba81b7dc232561b2
Vlad Tupchiienko <vt.progr@gmail.com>
2021-05-11 10:26:31 +0300

    Home Work 13 (Task 2)

--- src/homework13/task2/Main.java
+++ src/homework13/task2/Main.java
@@ -1,4 +1,20 @@
 package homework13.task2;
 
+import java.util.List;
+import java.util.concurrent.Semaphore;
+
 public class Main {
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(2);
+        List<Philosopher> philosopherList = List.of(
+                new Philosopher("Vladyslav", semaphore),
+                new Philosopher("Igor", semaphore),
+                new Philosopher("Volodymyr", semaphore),
+                new Philosopher("Oleksandr", semaphore),
+                new Philosopher("Kateryna", semaphore)
+        );
+        for (Philosopher philosopher : philosopherList) {
+            new Thread(philosopher).start();
+        }
+    }
 }
