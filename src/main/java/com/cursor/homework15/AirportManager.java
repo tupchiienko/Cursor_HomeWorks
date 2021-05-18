8805af772a7434fc399fe831a867d71d51652008
Vlad Tupchiienko <vt.progr@gmail.com>
2021-05-18 12:47:23 +0300

    Home Work 15 (fixed)

--- src/main/java/com/cursor/homework15/AirportManager.java
+++ src/main/java/com/cursor/homework15/AirportManager.java
@@ -21,18 +21,49 @@ public class AirportManager {
         var modelPlaneMap = planeList.stream().collect(Collectors.groupingBy(Plane::getModel));
         var modelPilotMap = pilotList.stream().collect(Collectors.groupingBy(Pilot::getPlaneModel));
         var clientCount = 0;
-        var busyPilots = new ArrayList<Pilot>();
-        for (Map.Entry<String, List<Pilot>> entry : modelPilotMap.entrySet()) {
-            var planeModel = entry.getKey();
-            var pilots = entry.getValue();
-            pilots.removeAll(busyPilots);
-            int planesCount = (pilots.size() % 2 == 0) ? pilots.size() / 2 : (pilots.size() - 1) / 2;
-            for (var i = 0; i < planesCount * 2; i++) {
-                busyPilots.add(pilots.get(i));
-            }
-            clientCount += planesCount * modelPlaneMap.get(planeModel).get(0).getSeats();
+        var usedPlanes = getUsedPlanesList(modelPlaneMap, modelPilotMap);
+        for (Plane plane : usedPlanes) {
+            clientCount += plane.getSeats();
         }
         return clientCount;
+    }
+
+    private List<Plane> getUsedPlanesList(Map<String, List<Plane>> modelPlaneMap,
+                                           Map<String, List<Pilot>> modelPilotMap) {
+        var usedPilots = new ArrayList<Pilot>();
+        var usedPlanes = new ArrayList<Plane>();
+        for (Map.Entry<String, List<Pilot>> modelPilotEntry : modelPilotMap.entrySet()) {
+            var planeModel = modelPilotEntry.getKey();
+            var pilots = modelPilotEntry.getValue();
+            pilots.removeAll(usedPilots);
+            int freePlanesCount = (pilots.size() % 2 == 0) ? pilots.size() / 2 : (pilots.size() - 1) / 2;
+            for (var i = 0; i < freePlanesCount * 2; i++) {
+                usedPilots.add(pilots.get(i));
+            }
+            fillUsedPlanesList(modelPlaneMap, usedPlanes, planeModel, freePlanesCount);
+        }
+        return usedPlanes;
+    }
+
+    private void fillUsedPlanesList(Map<String, List<Plane>> modelPlaneMap,
+                                    ArrayList<Plane> usedPlanes,
+                                    String planeModel,
+                                    int freePlanesCount) {
+        for (Map.Entry<String, List<Plane>> modelPlaneEntry : modelPlaneMap.entrySet()) {
+            if (modelPlaneEntry.getKey().equals(planeModel)) {
+                var planes = modelPlaneEntry.getValue();
+                planes.removeAll(usedPlanes);
+                for (Plane plane : planes) {
+                    if (plane.getModel().equals(planeModel)) {
+                        if (freePlanesCount == 0) {
+                            break;
+                        }
+                        usedPlanes.add(plane);
+                        freePlanesCount--;
+                    }
+                }
+            }
+        }
     }
 
     private List<Pilot> getPilotList() {
